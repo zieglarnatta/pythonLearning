fa2c365ed53fe9b0b1c4c5b0377fb69f660782b1
royyap1 <royyap1@gmail.com>
2020-02-05 08:01:25 -0800

    adding 3 files

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+class Palindrome():
+    def palindrome():
+        testword1 = 'tacocat'
+        testword2 = 'simplicity'
+        j = len (testword1)
+        i = len(testword1)+1
+        k = 0
+        l = j
+        m = i/2
+        while k < m
+            l > m
+            if testword[k] == testword[l]
+
+if __name__ == '__main__':
+  print(approximate_size(1000000000000, False))
+  print(approximate_size(1000000000000))
